f9227e5c56a9b46defe8ef80f747f62a07481ddd
Kalin K <kalin.kuzev@city.ac.uk>
2025-05-09 11:05:53 +0100

    FINAL

---
+++ stats.py
@@ -0,0 +1,71 @@
+#Variables
+fps=60
+menu=1
+ene=1
+gameover=0
+#Tutorial variables
+tutorialcheck=1
+arrowtutorial=1
+mousetutorial=1
+#Menu variables
+upgradesmenu=0
+quitmenu=0
+loadcontrol=0
+barx=1
+#Map and generation variables
+newmap=1
+movelayer=1
+moveroom=-1
+mapwidth=3
+mapheight=3
+floor=1
+floorcomplete=0
+lockedroom=False
+#Player variables
+sprcontrol=1
+sprcount=0
+playerx=352
+playery=277
+invincibleframe=60
+chosenupgrade=0
+#Permanent upgrades
+permstrength=0
+permmaxhp=0
+permmovespeed=0
+#Player health and max health values
+hpbarx=100
+maxhpbarx=100
+#####
+playerdmg=15
+flip=False
+movespeed=8
+direction=""
+#Default player stat values
+defaultmaxhp=100
+defaultplayerdmg=15
+defaultmovespeed=8
+#Enemy variables
+enemies=[]
+enemycount=0
+enemyflip=False
+enemyspawnlayer=1
+enemyspawnroom=-1
+enemyclearconfirm=0
+#Metaprogression
+currency=0
+#colours
+white=(255,255,255)
+transparent=(0,0,0,255)
+black=(0,0,0)
+grey=(169,169,169)
+green=(0,255,0)
+slgreen=(0,225,0)
+coolgreen=(100,200,100)
+slcoolgreen=(100,180,100)
+coolred=(200,100,100)
+slcoolred=(230,100,100)
+critred=(87,29,29)
+lblue=(173,216,230)
+gold=(255,215,0)
+dgold=(231,138,44)
+normal=(37,19,26)
